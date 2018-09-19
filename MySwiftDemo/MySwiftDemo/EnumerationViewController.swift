822224ab31caed3b87655c6f022371ca1f6930e3
Nick <walkmanaly@gmail.com>
2018-09-19 16:16:42 +0800

    enumeration
    enumeration

---
+++ MySwiftDemo/MySwiftDemo/EnumerationViewController.swift
@@ -0,0 +1,57 @@
+//
+//  EnumerationViewController.swift
+//  MySwiftDemo
+//
+//  Created by Nick on 2018/9/19.
+//  Copyright © 2018年 nick. All rights reserved.
+//
+
+import UIKit
+
+class EnumerationViewController: UIViewController {
+
+    enum season {
+        case spring
+        case summer
+        case fall
+        case winter
+    }
+    
+//    enum Tea: CaseIterable {
+//        case milktea, greentea, blacktea
+//
+//    }
+    
+    var now = season.spring
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.gray
+        // Do any additional setup after loading the view.
+        testEnum()
+    }
+    
+    func testEnum() {
+//        now  = .fall
+        switch now {
+        case .spring:
+            print("it is spring")
+        
+        case .summer:
+            print("it is summer")
+            
+        case .fall:
+            print("it is fall")
+            
+        case .winter:
+            print("it is winter")
+    
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- MySwiftDemo/MySwiftDemo/ScrollViewDemo/DemoScrollViewController.swift
+++ MySwiftDemo/MySwiftDemo/ScrollViewDemo/DemoScrollViewController.swift
@@ -27,6 +27,20 @@ class DemoScrollViewController: UIViewController {
         var second = 50
         swapTwoInts(&first, &second)
         print("first=\(first) second=\(second)")
+        
+        let encrementer = makeEncrementer(input: 5)
+        let rest = encrementer()
+        let rest1 = encrementer()
+        let rest2 = encrementer()
+        // 捕获变量，所以每次调用totalNum都会在原来值的基础上增加
+        print("result = \(rest) \(rest1) \(rest2)")
+        
+        let encrementer10 = makeEncrementer(input: 10)
+        let result0 = encrementer10()
+        let result1 = encrementer10()
+        let result2 = encrementer10()
+        // 捕获的变量跟上面的没影响，捕获各自的变量
+        print("result = \(result0) \(result1) \(result2)")
     }
 
     // inout 把传进来的参数做修改（修改外部变量）注意：传变量的引用
@@ -63,6 +77,16 @@ class DemoScrollViewController: UIViewController {
             return input + 1
         }
         return isBackword ? backWord : forword
+    }
+    
+    // 捕获变量
+    func makeEncrementer(input amount: Int) -> () -> Int {
+        var totalNum = 0
+        func encrementter() -> Int {
+            totalNum += amount
+            return totalNum
+        }
+        return encrementter
     }
 
 }
