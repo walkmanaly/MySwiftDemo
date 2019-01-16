1fe0d3c9c7d8a231039bef620aaa0977888592cd
Nick <walkmanaly@gmail.com>
2019-01-17 00:07:30 +0800

    计算型属性

---
+++ MySwiftDemo/MySwiftDemo/ComputedPropertyTest.swift
@@ -0,0 +1,29 @@
+//
+//  ComputedPropertyTest.swift
+//  MySwiftDemo
+//
+//  Created by Nick on 2019/1/16.
+//  Copyright © 2019 nick. All rights reserved.
+//
+
+import Foundation
+
+class MyFeture {
+    var height: Float = 172.0
+    var weight: Float = 62.0
+    
+    // 计算型属性并不存储任何值，当它被访问的时候，调用get去计算某个值再返回；(计算属性的值，得到结果)
+    //                      当它被赋值的时候，调用set去设置该对象的属性。
+    var heightWithChange: Float {
+        get {
+            return self.height + 10
+        }
+        set {
+            self.height = self.height + newValue
+        }
+    }
+}
+
+
+
+
