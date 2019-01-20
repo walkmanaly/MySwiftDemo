a6838af655e57371fc79f9807c1d4cb87f7246eb
Nick <walkmanaly@gmail.com>
2019-01-20 23:53:07 +0800

    使用GYB按照一定规则生成所需要的代码，实测有效。 https://nshipster.cn/swift-gyb/

---
+++ MySwiftDemo/MySwiftDemo/TestGYB.swift
@@ -0,0 +1,12 @@
+
+
+class MyCharacter {
+  var name: String?
+
+  var strength: Int?
+  var dexterity: Int?
+  var constitution: Int?
+  var intelligence: Int?
+  var wisdom: Int?
+  var charisma: Int?
+}
